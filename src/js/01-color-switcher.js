34310dfbc11dba0cfe27110ac9209136c21d514c
IvanPanin675 <jovanni.94.van@gmail.com>
2022-11-24 17:28:07 +0200

    task01-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const body = document.querySelector('body');
+let timerId = null;
 
+body.addEventListener('click', onBtnClick);
+
+function onBtnClick(e) {
+    if (e.target.textContent === 'Start') {
+        timerId = setInterval(() => {
+            body.style.backgroundColor = `${getRandomHexColor()}`;
+        }, 1000);
+        e.target.setAttribute('disabled', true);
+    }; 
+    if (e.target.textContent === 'Stop') {
+        clearInterval(timerId); 
+        e.target.previousElementSibling.removeAttribute('disabled');
+    }; 
+    
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
