dbf215ebca990b27b982d29d752460845fd32878
IvanPanin675 <jovanni.94.van@gmail.com>
2022-11-28 02:49:30 +0200

    task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } 
+        reject({ position, delay });
+    }, delay);
+  });
+};
+
+const refs = {
+  form: document.querySelector('.form'),
+  delayEl: document.querySelector('input[name = "delay"]'),
+  stepEl: document.querySelector('input[name = "step"]'),
+  amountEl: document.querySelector('input[name = "amount"]')
+};
+
+refs.form.addEventListener('submit', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  for (let i = 0; i < Number(refs.amountEl.value); i ++) {
+    createPromise(i + 1, i * Number(refs.stepEl.value) + Number(refs.delayEl.value))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
-}
+  e.target.reset();
+};
+
+
+console.log(refs.delayEl);
+console.log(refs.stepEl);
+console.log(refs.amountEl);
